9205ebda89aba42d3f166125ac1ac685fbb7f7ee
akskokki <akseli.kokki@gmail.com>
2022-12-30 17:18:05 +0200

    Separate blog model

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -6,17 +6,16 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
