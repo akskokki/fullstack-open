6d22ec85699752391c2e64b6c60d77471081b584
akskokki <akseli.kokki@gmail.com>
2022-12-30 17:28:02 +0200

    Separate app

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,29 +1,10 @@
-const config = require('./utils/config')
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-const Blog = require('./models/blog')
-const blogsRouter = require('./controllers/blogs')
+const server = http.createServer(app)
 
-logger.info('connecting to', config.MONGODB_URI)
-mongoose.connect(config.MONGODB_URI)
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT || 3003
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
