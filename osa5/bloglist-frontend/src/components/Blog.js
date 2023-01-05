515ea71680225f625f4d0737cd43f56c48b51c3e
akskokki <akseli.kokki@gmail.com>
2023-01-05 18:47:16 +0200

    Add exercise 5.7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [viewDetails, setViewDetails] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const details = () => (
+    <div>
+      <div>{blog.url}</div>
+      <div>{blog.likes} <button>like</button></div>
+      <div>{blog.user.name ? blog.user.name : blog.user.username}</div>
+    </div>
+  )
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={(() => setViewDetails(!viewDetails))}>view</button>
+      </div>
+      {viewDetails && details()}
+    </div>
+  )
+}
 
 export default Blog
