aa02142eb8b3f47ada36e792dcb0fd94cd9b263d
akskokki <akseli.kokki@gmail.com>
2023-01-10 23:59:08 +0200

    Add exercise 5.15

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -5,12 +5,14 @@ import Blog from './Blog'
 import userEvent from '@testing-library/user-event'
 
 describe('<Blog />', () => {
+  const mocks = {}
   beforeEach(() => {
     const blog = {
       title: 'testTitle',
       author: 'testAuthor',
       url: 'testUrl',
       likes: 42,
+      id: 'testId',
       user: {
         username: 'testUser',
         name: 'testName'
@@ -20,6 +22,9 @@ describe('<Blog />', () => {
     const mockLike = jest.fn()
     const mockRemove = jest.fn()
     const loggedUser = { username: 'loggedUser' }
+
+    mocks.like = mockLike
+    mocks.remove = mockRemove
 
     render(<Blog blog={blog} likeBlog={mockLike} loggedUser={loggedUser} removeBlog={mockRemove} />)
   })
@@ -39,5 +44,19 @@ describe('<Blog />', () => {
     screen.getByText('testUrl')
     screen.getByText('42')
     screen.getByText('testName')
+  })
+
+  test('like button can be pressed twice', async () => {
+    const user = userEvent.setup()
+    const detailsButton = screen.getByText('view')
+
+    await user.click(detailsButton)
+
+    const likeButton = screen.getByText('like')
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mocks.like.mock.calls).toHaveLength(2)
   })
 })
