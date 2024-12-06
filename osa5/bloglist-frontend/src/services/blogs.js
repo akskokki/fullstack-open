b87d27e373976444f0101bcf9cad5536b52cf5d0
akskokki <akseli.kokki@gmail.com>
2024-12-06 22:04:13 +0200

    Restart osa5

--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -1,49 +1,9 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
-let token = null
-
-const setToken = newToken => {
-  token = `bearer ${newToken}`
+const getAll = () => {
+  const request = axios.get(baseUrl)
+  return request.then(response => response.data)
 }
 
-const getAll = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-}
-
-const create = async newBlog => {
-  const config = {
-    headers: { Authorization: token }
-  }
-
-  const response = await axios.post(baseUrl, newBlog, config)
-  return response.data
-}
-
-const addLike = async (blog) => {
-  const blogUrl = baseUrl + '/' + blog.id
-
-  const likedBlog = {
-    user: blog.user.id,
-    likes: blog.likes + 1,
-    author: blog.author,
-    title: blog.title,
-    url: blog.url
-  }
-
-  const response = await axios.put(blogUrl, likedBlog)
-  return response.data
-}
-
-const remove = async blogId => {
-  const blogUrl = `${baseUrl}/${blogId}`
-  const config = {
-    headers: { Authorization: token }
-  }
-
-  await axios.delete(blogUrl, config)
-}
-
-//eslint-disable-next-line
-export default { getAll, setToken, create, addLike, remove }
+export default { getAll }
