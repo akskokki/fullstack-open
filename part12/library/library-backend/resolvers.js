8ec042f9832c541d680e245e0f0826bbf8004a10
akskokki <akseli.kokki@gmail.com>
2025-01-06 22:33:18 +0200

    Add exercise 12.21

--- part12/library/library-backend/resolvers.js
+++ part12/library/library-backend/resolvers.js
@@ -1,5 +1,6 @@
 const { GraphQLError } = require('graphql')
 const { PubSub } = require('graphql-subscriptions')
+const jwt = require('jsonwebtoken')
 
 const User = require('./models/user')
 const Book = require('./models/book')
