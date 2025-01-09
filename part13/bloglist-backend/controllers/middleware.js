f5541e3c70ade1b0ef97b5a9b6c35527d9c1287b
akskokki <akseli.kokki@gmail.com>
2025-01-09 16:36:19 +0200

    Add exercise 13.9

--- part13/bloglist-backend/controllers/middleware.js
+++ part13/bloglist-backend/controllers/middleware.js
@@ -1,4 +1,9 @@
+const { ValidationError } = require('sequelize')
+
 const errorHandler = (error, req, res, next) => {
+  if (error instanceof ValidationError) {
+    return res.status(400).json({ error: error.errors.map((e) => e.message) })
+  }
   res.status(400).json({ error })
 }
 
